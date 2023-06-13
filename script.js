aa07db6ff232550a924c42c5795919f06b01c975
Zain~Sama <abdulrahmanzain32@gmail.com>
2023-06-13 16:08:31 +0200

    basic DOM declarations

---
+++ script.js
@@ -0,0 +1,10 @@
+const screen = document.getElementById("screen");
+const btns = document.getElementById("btns");
+
+btns.addEventListener('click', (e) => {
+    let target = e.target;
+    let value = target.value;
+    if(value != undefined){
+    screen.innerHTML = value;
+    }
+})
