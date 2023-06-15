02f5f8f83d04cc29c148ad42eb1a1bdc968883ef
Zain~Sama <abdulrahmanzain32@gmail.com>
2023-06-15 21:51:11 +0200

    added logic for deleting btns

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const screen = document.getElementById("screen");
+const screenText = document.getSelection("screen");
 const btns = document.getElementById("btns");
 
-// btns.addEventListener('click', (e) => {
-//     let target = e.target;
-//     let value = target.value;
-//     if(value != undefined){
-//         screen.innerHTML = value;
-//     }
-// })
+btns.addEventListener('click', (e) => {
+    let target = e.target;
+    let value = target.value;
+    if(value != undefined && value != 'AC' && value != 'DE' && value != '='){
+        screen.innerHTML += value;  
+    };
+    if(value === 'AC'){
+        screen.innerHTML = ''
+    };
+    if(value === 'DE') {
+        screen.innerHTML = screen.textContent.slice(0, -1);
+    };
+});
