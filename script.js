1d318239951a95761119c3eb10899e9c633a288f
Zain~Sama <abdulrahmanzain32@gmail.com>
2023-06-17 20:57:31 +0200

    erased operate func

--- script.js
+++ script.js
@@ -26,26 +26,10 @@ btns.addEventListener('click', (e) => {
         output.innerHTML = '';
     }
     if(value === '=') {
+        console.log(input.textContent);
         output.innerHTML = operate(input.textContent);
     };
 });
 
 
-function operate(operation) {
-    operation = operation.split(' ');
-    
-    while(operation.includes(' * ') || operation.includes(' / ')) {
-        for(let i = 0; i < operation.length; i++){
-            if(operation[i] === ' * ') {
-                Number(operation[i - 1]) *= Number(operation[i + 1]);
-                operation.splice(i + 1, 1);
-                operation.splice(i - 1, 1);
-            }
-            if(operation[i] === '/') {
-                operation[i] = operation[i - 1] / operation[i + 1];
-                operation.splice(i + 1, 1)
-                operation.splice(i - 1, 1)
-            }
-        }
-    }
-}
+function operate(operation) {}
