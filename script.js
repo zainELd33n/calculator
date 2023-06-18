cab9f4cdb2ee2bb5fd23582e0566543a57c66c36
Zain~Sama <abdulrahmanzain32@gmail.com>
2023-06-18 13:48:53 +0200

    calculator logic added

--- script.js
+++ script.js
@@ -26,10 +26,57 @@ btns.addEventListener('click', (e) => {
         output.innerHTML = '';
     }
     if(value === '=') {
-        console.log(input.textContent);
         output.innerHTML = operate(input.textContent);
     };
 });
 
 
-function operate(operation) {}
+    function operate(operation) {
+        // turning operation into an array
+        operation = operation.split(' ')
+      
+        // stores the result of operation
+        let operationResult;
+        // this loop will go through every element of the array and operates every * or / operation and shrinks the string down until there is no more * or / left
+        while (operation.includes('/') || operation.includes('*')) {
+          for (let i = 0; i < operation.length; i++) {
+            if (operation[i] === '*') {
+              operation[i] = operation[i + 1];
+              operation[i] *= operation[i - 1];
+              operation.splice(i + 1, 1)
+              operation.splice(i - 1, 1)
+            }
+            if (operation[i] === '/') {
+              operation[i] = operation[i - 1] / operation[i + 1];
+              operation.splice(i + 1, 1)
+              operation.splice(i - 1, 1)
+            }
+          }
+        }
+        // this loop will go through every element of the array and operates every - or + operation and shrinks the string down until there is no more - or + left
+        while (operation.includes('+') || operation.includes('-')) {
+          for (let i = 0; i < operation.length; i++) {
+            if (operation[i] === '+') {
+              operation[i + 1] = Number(operation[i + 1]);
+              operation[i - 1] = Number(operation[i - 1]);
+              operation[i] = operation[i + 1];
+              operation[i] += operation[i - 1];
+              operation.splice(i + 1, 1)
+              operation.splice(i - 1, 1)
+            }
+            if (operation[i] === '-') {
+              operation[i] = operation[i - 1];
+              operation[i] -= operation[i + 1];
+              operation.splice(i + 1, 1)
+              operation.splice(i - 1, 1)
+            }
+          }
+        }
+        /* Checking to see if the result have a decimal point if so round it up to two decimal points for better readability else just set operation result to it's original value*/
+        if (operation[0] % 1 !== 0) {
+          operationResult = Math.round(operation[0] * 100) / 100;
+        } else {
+          operationResult = operation[0];
+        }
+        return operationResult;
+      }
